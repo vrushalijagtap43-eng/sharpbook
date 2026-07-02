6ef2c6505bd9106d1ff3d8e44e1a79bb2c74a4d3
vrushalijagtap43 <vrushalijagtap43@gmail.com>
2026-07-02 14:40:25 +0530

    all

--- sharptest/views.py
+++ sharptest/views.py
@@ -76,19 +76,31 @@ class PackageCrud(APIView):
     #     serializer = PackageSerializer(package, many=True)
     #     return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def get(self,request):
-        search = request.query_params.get("search")
-        brand_id = request.query_params.get("brand_id")
+    # def get(self,request):
+    #     search = request.query_params.get("search")
+    #     brand_id = request.query_params.get("brand_id")
+    #
+    #     package = Package.objects.all()
+    #
+    #     if search:
+    #         package = package.filter(name__icontains = search)
+    #     if brand_id:
+    #         package =package.filter(brand_id = brand_id)
+    #
+    #     serializers = PackageSerializer(package,many = True)
+    #     return Response(serializers.data,status = status.HTTP_200_OK)
 
+    # JIRA Ticket: SHARP-103 – Sorting API
+
+    def get(self,request):
+        sort=request.query_params.get("sort")
         package = Package.objects.all()
 
-        if search:
-            package = package.filter(name__icontains = search)
-        if brand_id:
-            package =package.filter(brand_id = brand_id)
+        if sort:
+            package=package.order_by(sort)
 
-        serializers = PackageSerializer(package,many = True)
-        return Response(serializers.data,status = status.HTTP_200_OK)
+        serializer = PackageSerializer(package,many = True)
+        return Response(serializer.data,status = status.HTTP_200_OK)
 
     def post(self,request):
         serializer=PackageSerializer(data=request.data)
