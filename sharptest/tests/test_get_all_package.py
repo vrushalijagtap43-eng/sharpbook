af960f6950f522b33eb3d44c31d5787b77565b0b
vrushalijagtap43 <vrushalijagtap43@gmail.com>
2026-07-09 17:07:39 +0530

    sharp-104 done

--- sharptest/tests/test_get_all_package.py
+++ sharptest/tests/test_get_all_package.py
@@ -1,12 +1,21 @@
 import pytest
+from decimal import Decimal
 # By default, pytest only discovers test files whose names start with test_ or end with _test.py.
 @pytest.mark.django_db
 def test_get_all_package(api_client,package):
     response = api_client.get("/package/")
     assert response.status_code==200
     assert len(response.data)==1
-#     In Python, assert is used to verify that a condition is True. It is commonly
-#     used in testing (especially with pytest) to check whether the actual result matches the expected result
+
+    data = response.data[0]
+
+    assert data["name"] == package.name
+    assert data["description"] == package.description
+    # assert data["price"] == package.price
+    assert Decimal(data["price"]) == package.price
+    assert Decimal(data["discount_percentage"]) == package.discount_percentage
+    assert data["image"] == package.image
+    assert data["brand"] == package.brand.id
 
 
 
